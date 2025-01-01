9a58b5c8817be4c67a6752a7522e857150fa065f
Luca Félix <zil1st@bosch.com>
2025-01-01 15:32:59 +0100

    refactor: :recycle: format next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [{
-            protocol: "https",
-            hostname: "avatars.githubusercontent.com",
-        }]
-    }
-};
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+      },
+    ],
+  },
+}
 
-export default nextConfig;
+export default nextConfig
